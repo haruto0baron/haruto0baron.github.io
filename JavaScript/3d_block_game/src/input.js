23549d5d8b066984a9a5bd2a1e33e6b326da72d8
haruto0baron <haru_to0baron@outlook.jp>
2026-04-11 07:38:31 +0900

    Delete JavaScript/3d_block_game directory

--- JavaScript/3d_block_game/src/input.js
+++
@@ -1,8 +0,0 @@
-export class Input {
-  constructor() {
-    this.keys = {};
-
-    window.addEventListener("keydown", e => this.keys[e.key.toLowerCase()] = true);
-    window.addEventListener("keyup", e => this.keys[e.key.toLowerCase()] = false);
-  }
-}
--- JavaScript/3d_block_game/src/save.js
+++
@@ -1,9 +0,0 @@
-export class Save {
-  static save(world) {
-    localStorage.setItem("world", JSON.stringify(world));
-  }
-
-  static load() {
-    return JSON.parse(localStorage.getItem("world") || "{}");
-  }
-}
